7b22fc685c1a07284736f9a7e9afe4da3f0d871b
Gustavo de Araujo Oliveira <gustavo.dearaujo26@gmail.com>
2024-09-30 15:47:07 -0300

    Add project description to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,6 +4,14 @@ import { titleReducer } from '../../state/title/title.reducer';
 import { AppState } from '../../state/app.state';
 import { addTitle } from '../../state/title/title.actions';
 import { StockCarouselComponent } from '../../components/stock-carousel/stock-carousel.component';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { StockCodeDisplayComponent } from '../../components/stock-code-display/stock-code-display.component';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +19,15 @@ import { StockCarouselComponent } from '../../components/stock-carousel/stock-ca
   styleUrls: ['./home.component.css'],
   standalone: true,
   imports: [
-    StockCarouselComponent
+    StockCarouselComponent,
+    MatCardModule,
+    MatIconModule,
+    CommonModule,
+    MatTooltipModule,
+    MatButtonModule,
+    StockCodeDisplayComponent,
+    MatDividerModule,
+    MatProgressBarModule
   ]
 })
 export class HomeComponent implements OnInit {
