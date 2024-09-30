7b4d1ba00f9639d55159a4f9bf42396b0470d95c
Gustavo de Araujo Oliveira <gustavo.dearaujo26@gmail.com>
2024-09-30 15:22:01 -0300

    Finish stock carousel development

--- src/app/components/stock-carousel/stock-carousel.component.ts
+++ src/app/components/stock-carousel/stock-carousel.component.ts
@@ -36,11 +36,13 @@ export class StockCarouselComponent implements OnInit {
   stocks: OptionStock[] = [];
   carouselStocks: OptionStock[] = [];
 
-  carouselInterval: number = 2000;
+  carouselInterval: number = 1000;
   startIndex: number = 0;
   carouselItemCount: number = 5;
   carouselSubscription: Subscription | null = null;
+  carouselIntervalStopped: boolean = false;
   mobileDevice: boolean = false;
+  carouselFirstExecution: boolean = true;
 
   constructor(private store: Store<AppState>, private deviceService: DeviceDetectorService) {
     this.mobileDevice = this.deviceService.isMobile();
@@ -60,14 +62,67 @@ export class StockCarouselComponent implements OnInit {
   }
 
   startCarousel() {
-    this.carouselSubscription = interval(5000).subscribe(val => this.loadCarouselItems());
+    this.carouselSubscription = interval(this.carouselInterval).subscribe(val => {
+      if (!this.carouselFirstExecution){
+        this.startIndex = this.calculateIndex(this.startIndex, true);
+      }
+
+      this.loadCarouselItems(this.startIndex, this.carouselItemCount);
+      this.carouselFirstExecution = false;
+    });
   }
 
-  loadCarouselItems() {
-    this.carouselStocks = this.stocks.slice(this.startIndex, this.startIndex + this.carouselItemCount);
-    this.startIndex = this.startIndex + 1;
-    if (this.startIndex >= this.stocks.length){
-      this.startIndex = 0;
+  loadCarouselItems(currentIndex: number, itemCount: number) {
+    var itemsToGet = currentIndex + itemCount;
+
+    var capturedStocks = this.stocks.slice(currentIndex, itemsToGet);
+    if (capturedStocks.length < itemCount){
+      var diff = itemCount - capturedStocks.length;
+
+      for(var i = 0; i < diff; i++){
+        capturedStocks.push(this.stocks[i]);
+      }
     }
+
+    this.carouselStocks = capturedStocks;
+  }
+
+  goToLeft() {
+    if (!this.carouselSubscription!.closed){
+      this.carouselSubscription?.unsubscribe();
+    }
+
+    this.startIndex = this.calculateIndex(this.startIndex, false);
+    this.loadCarouselItems(this.startIndex, this.carouselItemCount);
+  }
+
+  gotToRight() {
+    if (!this.carouselSubscription!.closed){
+      this.carouselSubscription?.unsubscribe();
+    }
+
+    this.startIndex = this.calculateIndex(this.startIndex, true);
+    this.loadCarouselItems(this.startIndex, this.carouselItemCount);
+  }
+
+  calculateIndex(currentIndex: number, moveFoward: boolean): number{
+    var newIndex = 0;
+
+    var lastStockIndex = this.stocks.length - 1;
+
+    if (moveFoward && currentIndex < lastStockIndex){
+      newIndex = currentIndex + 1;
+    }
+    else if (moveFoward && currentIndex === lastStockIndex){
+      newIndex = 0;
+    }
+    else if (!moveFoward && currentIndex === 0){
+      newIndex = lastStockIndex;
+    }
+    else if (!moveFoward && currentIndex > 0){
+      newIndex = currentIndex - 1;
+    }
+
+    return newIndex;
   }
 }
